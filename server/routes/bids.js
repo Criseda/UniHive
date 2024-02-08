315b80e0bbb864e53fa6a09b34bef18043c02ec1
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-02-08 13:26:12 +0000

    Add skeleton routes for bids, listings, reports, and reviews

---
+++ server/routes/bids.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../db");
+
+// define routes for bids(on listings) here
+
+module.exports = router;
---
+++ server/routes/listings.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../db");
+
+// define routes for listings(buy now stuff) here
+
+module.exports = router;
