003c84300ccc9da01e1475ee416ec860d8247591
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-02-08 11:02:09 +0000

    renamed index.js to app.js (entry point of server)

--- server/app.js
+++ server/app.js
@@ -2,10 +2,20 @@
 // this file is run by node
 // run prettier with 2 space formatting on this file
 const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+
 const app = express();
+
+app.use(bodyParser.json());
+
+// use the routes in the routes file
+app.use('/api', routes);
+
+
 
 app.listen(5000, () => {
   console.log("server is listening on port 5000");
 });
 
-//run the server with the command: nodemon index
+//run the server with the command: nodemon app
