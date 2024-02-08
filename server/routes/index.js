9e269c8ea9b217eaa031af540226965b6856f49c
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-02-08 13:03:31 +0000

    Enable profiles and auctions routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const usersRouter = require('./users');
-// const profilesRouter = require('./profiles');
-// const auctionsRouter = require('./auctions');
+const profilesRouter = require('./profiles');
+const auctionsRouter = require('./auctions');
 
 const router = express.Router();
 
 router.use('/users', usersRouter);
-// router.use('/profiles', profilesRouter);
-// router.use('/auctions', auctionsRouter);
+router.use('/profiles', profilesRouter);
+router.use('/auctions', auctionsRouter);
 
 module.exports = router;
