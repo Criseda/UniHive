206682f7d3b05e9bb2e3a43634638d77dba1a0d9
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-02-13 21:24:14 +0000

    Add API integration and dynamic data loading to Itemlist component

--- client/src/components/Itemlist.jsx
+++ client/src/components/Itemlist.jsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getItems } from "../api/items";
 
 const Itemlist = () => {
   // Dummy data for demonstration purposes
-  const items = [
-    { id: 1, name: "Item 1", price: "$19.99", imageUrl: "/images/yay.jpg" },
-    { id: 2, name: "Item 2", price: "$29.99", imageUrl: "/images/the_thinker.jpg" },
-    { id: 3, name: "Item 3", price: "$39.99", imageUrl: "/images/yay.jpg" },
-    { id: 4, name: "Item 4", price: "$39.99", imageUrl: "/images/yay.jpg" },
-    { id: 5, name: "Item 5", price: "$39.99", imageUrl: "/images/crazy_businesswoman.jpeg" },
-    { id: 6, name: "Item 6", price: "$39.99", imageUrl: "/images/default_pfp.jpg" },
-    // Add more items as needed
-  ];
+  // const items = [
+  //   // { id: 1, name: "Item 1", price: "$19.99", imageUrl: "/images/yay.jpg" },
+  //   // { id: 2, name: "Item 2", price: "$29.99", imageUrl: "/images/the_thinker.jpg" },
+  //   // { id: 3, name: "Item 3", price: "$39.99", imageUrl: "/images/yay.jpg" },
+  //   // { id: 4, name: "Item 4", price: "$39.99", imageUrl: "/images/yay.jpg" },
+  //   // { id: 5, name: "Item 5", price: "$39.99", imageUrl: "/images/crazy_businesswoman.jpeg" },
+  //   // { id: 6, name: "Item 6", price: "$39.99", imageUrl: "/images/default_pfp.jpg" },
+  //   // Add more items as needed
+  // ];
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getItems()
+      .then((data) => {
+        setItems(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <div className="container mt-4">
