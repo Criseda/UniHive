a6625129e45e614ebb125b0798487776a24706ee
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-02-13 21:23:59 +0000

    Add CORS support to the server

--- server/app.js
+++ server/app.js
@@ -4,9 +4,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 // use the routes in the routes file
