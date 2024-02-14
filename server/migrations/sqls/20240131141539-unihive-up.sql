8455b471d8df3e69dc32111fc889a3be7dd6c843
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-02-14 18:48:46 +0000

    Revised database: combined app_user and profile tables, added "banned" field

--- server/migrations/sqls/20240131141539-unihive-up.sql
+++ server/migrations/sqls/20240131141539-unihive-up.sql
@@ -4,31 +4,25 @@
 
 CREATE TABLE app_user (
     id SERIAL PRIMARY KEY,
-    username CHAR(6) NOT NULL UNIQUE,
+    username CHAR(6) NOT NULL UNIQUE, --unique username given by API, fixed 6 characters
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE profile (
-    id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL UNIQUE,
-    rating INT,
+    rating INT, --rating of the user (calculated by averaging all reviews, init null)
     bio VARCHAR(255),
-    avatar_path VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES app_user (id) ON DELETE CASCADE ON UPDATE CASCADE
+    avatar_path VARCHAR(255) NOT NULL, --url of the image for the avatar
+    banned BOOLEAN DEFAULT FALSE, --if the user is banned
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE auction (
     id SERIAL PRIMARY KEY,
-    seller_id INT NOT NULL,
+    seller_id INT NOT NULL, --profile of the seller
     name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     opening_bid NUMERIC(12,2) NOT NULL,
     closing_date TIMESTAMP NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (seller_id) REFERENCES profile (id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (seller_id) REFERENCES app_user (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE bid (
@@ -48,7 +42,7 @@ CREATE TABLE listing (
     description TEXT NOT NULL,
     price NUMERIC(12,2) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (seller_id) REFERENCES profile (id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (seller_id) REFERENCES app_user (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE message (
@@ -61,17 +55,18 @@ CREATE TABLE message (
     FOREIGN KEY (receiver_id) REFERENCES app_user (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+-- reviews table
 CREATE TABLE review (
     id SERIAL PRIMARY KEY,
     reviewer_id INT NOT NULL,
-    reviewee_id INT NOT NULL,
+    reviewed_id INT NOT NULL,
     rating INT NOT NULL,
     review TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (reviewer_id) REFERENCES app_user (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (reviewee_id) REFERENCES profile (id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (reviewed_id) REFERENCES app_user (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
+-- reports table
 CREATE TABLE report (
     id SERIAL PRIMARY KEY,
     reporter_id INT NOT NULL,
@@ -81,17 +76,17 @@ CREATE TABLE report (
     FOREIGN KEY (reporter_id) REFERENCES app_user (id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (reported_id) REFERENCES app_user (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
+-- images tables (split because an image cannot be associated with both an auction and a listing at the same time)
 CREATE TABLE listing_image (
     id SERIAL PRIMARY KEY,
-    image_path VARCHAR(255) NOT NULL,
+    image_path VARCHAR(255) NOT NULL, --url to image
     listing_id INT REFERENCES listing (id) ON DELETE CASCADE ON UPDATE CASCADE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE auction_image (
     id SERIAL PRIMARY KEY,
-    image_path VARCHAR(255) NOT NULL,
+    image_path VARCHAR(255) NOT NULL, --url to image
     auction_id INT REFERENCES auction (id) ON DELETE CASCADE ON UPDATE CASCADE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
