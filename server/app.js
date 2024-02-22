d0e8305d5e0189a26e0fb9a1c3c0229e339ca4dd
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-02-22 12:51:55 +0000

    Added session middleware and auth routes

--- server/app.js
+++ server/app.js
@@ -2,22 +2,32 @@
 // this file is run by node
 // run prettier with 2 space formatting on this file
 const express = require("express");
-const bodyParser = require("body-parser");
+const session = require("express-session");
 const routes = require("./routes");
+const authRoutes = require("./auth/auth.js");
+
 const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(
+	session({
+		secret: "i am a secret", // change this to a random string in a .env file
+		resave: false,
+		saveUninitialized: true,
+		cookie: { secure: false },
+		// try set cookies to secure in production
+	})
+);
 
 // use the routes in the routes file
-app.use('/api', routes);
-
-
+app.use("/api", routes);
+app.use("/auth", authRoutes);
 
 app.listen(5000, () => {
-  console.log("server is listening on port 5000");
+	console.log("server is listening on port 5000");
 });
 
 //run the server with the command: nodemon app
