254a6ca96ccbec252fb07945fd26934a8c6a5c70
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-02-29 20:35:14 +0000

    Added dependency on having to be logged in to be able to access other parts of the application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
@@ -7,18 +7,22 @@ import Messages from "./routes/Messages";
 import Saved_items from "./routes/Saved_items";
 import AuthHandler from "./routes/AuthHandler";
 
-const App = () => {
+const ProtectedRoute = ({ element, ...rest }) => {
+  const isAuthenticated = sessionStorage.getItem("csticket") && sessionStorage.getItem("username") && sessionStorage.getItem("fullname");
+  return isAuthenticated ? element : <Navigate to="/login" />;
+};
 
+const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/item/:id" element={<Item />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/Saveditem" element={<Saved_items />} />
-        <Route path="/AuthHandler" element={<AuthHandler />} />
+        <Route path="/item/:id" element={<ProtectedRoute element={<Item />} />} />
+        <Route path="/messages" element={<ProtectedRoute element={<Messages />} />} />
+        <Route path="/Saveditem" element={<ProtectedRoute element={<Saved_items />} />} />
+        <Route path="/AuthHandler" element={<AuthHandler />} /> {/* AuthHandler route is not protected */}
       </Routes>
     </Router>
   );
