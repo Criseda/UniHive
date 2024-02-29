da0276b8cf7a06bc844cf1716724899976cab7ce
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-02-29 20:33:47 +0000

    Allowed everything CORS related on the server side

--- server/app.js
+++ server/app.js
@@ -10,11 +10,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({
-	origin: true,
-	credentials: true,
-	optionsSuccessStatus: 200
-}));
+app.use(cors());
 app.use(express.json());
 app.use(
 	session({
