3b5de0b4eda21322b72e4293cdff93659b0607c7
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-03-05 13:06:28 +0000

    Loading... doesn't appear anymore after changing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
 } from "react-router-dom";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
@@ -13,72 +13,70 @@ import Saved_items from "./routes/Saved_items";
 import AuthHandler from "./routes/AuthHandler";
 
 const ProtectedRoute = ({ element }) => {
-  const [authenticated, setAuthenticated] = useState(null);
+	const [authenticated, setAuthenticated] = useState(null);
 
-  // this useEffect will run everytime a user tries to access a protected route
-  // it will check if the user is authenticated with the backend API
-  // frontend will give the JWT it received back to the backend to check if it's valid
-  // if it is, the backend will respond with a 200 status code and all is well
-  useEffect(() => {
-    // Check user authentication
-    const checkAuthentication = async () => {
-      try {
-        // Make GET request to check authentication
-        const token = localStorage.getItem("token");
-        if (token) {
-          const response = await fetch("http://localhost:5000/auth/checkAuth", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ token }),
-          });
-          const data = await response.json();
-          // Set authenticated based on response
-          setAuthenticated(data.authenticated);
-        } else {
-          setAuthenticated(false);
-        }
-      } catch (error) {
-        console.error("Error checking authentication:", error);
-      }
-    };
+	// this useEffect will run everytime a user tries to access a protected route
+	// it will check if the user is authenticated with the backend API
+	// frontend will give the JWT it received back to the backend to check if it's valid
+	// if it is, the backend will respond with a 200 status code and all is well
+	useEffect(() => {
+		// Check user authentication
+		const checkAuthentication = async () => {
+			try {
+				// Make GET request to check authentication
+				const token = localStorage.getItem("token");
+				if (token) {
+					const response = await fetch("http://localhost:5000/auth/checkAuth", {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({ token }),
+					});
+					const data = await response.json();
+					// Set authenticated based on response
+					setAuthenticated(data.authenticated);
+				} else {
+					setAuthenticated(false);
+				}
+			} catch (error) {
+				console.error("Error checking authentication:", error);
+			}
+		};
 
-    checkAuthentication();
-  }, []);
+		checkAuthentication();
+	}, []);
 
-  if (authenticated === null) {
-    return <div>Loading...</div>;
-    // adding a cool loading screen would be nice here
-  }
+	if (authenticated === null) {
+		return null;
+	}
 
-  return authenticated ? element : <Navigate to="/login" />;
+	return authenticated ? element : <Navigate to="/login" />;
 };
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<ProtectedRoute element={<Home />} />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/item/:id"
-          element={<ProtectedRoute element={<Item />} />}
-        />
-        <Route
-          path="/messages"
-          element={<ProtectedRoute element={<Messages />} />}
-        />
-        <Route
-          path="/Saveditem"
-          element={<ProtectedRoute element={<Saved_items />} />}
-        />
-        <Route path="/AuthHandler" element={<AuthHandler />} />{" "}
-        {/* AuthHandler route is not protected */}
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Navigate to="/home" />} />
+				<Route path="/home" element={<ProtectedRoute element={<Home />} />} />
+				<Route path="/login" element={<Login />} />
+				<Route
+					path="/item/:id"
+					element={<ProtectedRoute element={<Item />} />}
+				/>
+				<Route
+					path="/messages"
+					element={<ProtectedRoute element={<Messages />} />}
+				/>
+				<Route
+					path="/Saveditem"
+					element={<ProtectedRoute element={<Saved_items />} />}
+				/>
+				<Route path="/AuthHandler" element={<AuthHandler />} />{" "}
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
