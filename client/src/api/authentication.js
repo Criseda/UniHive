5fc36e5e39bf103a95520bff2218b38d60015769
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-03-05 13:09:04 +0000

    removed redundant functions and added loginRoute and logoutRoute functions

--- client/src/api/authentication.js
+++ client/src/api/authentication.js
@@ -1,22 +1,5 @@
 const BASE_URL = 'http://localhost:5000/auth/';
 
-export function Login(){
-  return fetch(`${BASE_URL}/login`).then((res) => res.json());
-}
+export const logoutRoute = `${BASE_URL}/logout`;
 
-export function Logout(){
-  return fetch(`${BASE_URL}/logout`).then((res) => res.json());
-}
-
-export function checkAuth(){
-  return fetch(`${BASE_URL}/session`)
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.authenticated) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-}
-
+export const loginRoute = `${BASE_URL}/login`;
