7eed937399be51c35606c332e0cc02e96773618c
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-03-06 22:17:36 +0000

    Removed getItems and getItem from item.js routes

--- client/src/api/items.js
+++ client/src/api/items.js
@@ -1,28 +1,19 @@
-const BASE_URL = `http://${process.env.REACT_APP_IP_ADDRESS || "localhost"}:5000/api/`;
+const BASE_URL = `http://${
+  process.env.REACT_APP_IP_ADDRESS || "localhost"
+}:5000/api/`;
 
-//named wrong, not gonna change for now to prevent conflicts
-export function getItems() {
-  return fetch(`${BASE_URL}/listings/`).then((res) => res.json());
-  //TODO: CREATE A BACKEND ROUTE /ITEMS THAT COMBINES LISTINGS AND AUCTIONS
+export async function getListings() {
+  const res = await fetch(`${BASE_URL}/listings/`);
+  return await res.json();
 }
 
-export function getItem(id) {
-  return fetch(`${BASE_URL}/listings/${id}`).then((res) => res.json());
+export async function getListing(id) {
+  const res = await fetch(`${BASE_URL}/listings/${id}`);
+  return await res.json();
 }
 
 export async function getListingImages(id) {
   const res = await fetch(`${BASE_URL}/listing_images/listing/${id}`);
-  return await res.json();
-}
-
-export async function getAuctionImages(id) {
-  const res = await fetch(`${BASE_URL}/auction_images/auction/${id}`);
-  return await res.json();
-}
-
-// routes for itemlist.jsx
-export async function getListings() {
-  const res = await fetch(`${BASE_URL}/listings/`);
   return await res.json();
 }
 
@@ -36,8 +27,8 @@ export async function getAuction(id) {
   return await res.json();
 }
 
-export async function getListing(id) {
-  const res = await fetch(`${BASE_URL}/listings/${id}`);
+export async function getAuctionImages(id) {
+  const res = await fetch(`${BASE_URL}/auction_images/auction/${id}`);
   return await res.json();
 }
 
