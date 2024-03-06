a3990e72efe19e156c9a04d7731353c3c7a1bd83
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-03-06 19:13:05 +0000

    Added Search Bar component in Items.jsx route

--- client/src/routes/Item.jsx
+++ client/src/routes/Item.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Search from "../components/Search";
 import ItemDetails from "../components/ItemDetails";
 
 const Item = () => {
   return (
     <>
       <Navbar />
+      <Search />
       <ItemDetails />
     </>
   );
