a74ad1cd008877df077b8c035d76bb0d5bf79d97
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-03-11 19:33:05 +0000

    Added onClick event to Save button to call requests and add it to the users saved_items

--- client/src/components/ItemDetails.jsx
+++ client/src/components/ItemDetails.jsx
@@ -4,183 +4,203 @@ import AuctionBidCount from "./AuctionBidCount";
 import { Carousel, Button } from "react-bootstrap";
 import { useParams, Link } from "react-router-dom";
 import {
-	getListing,
-	getAuction,
-	getListingImages,
-	getAuctionImages,
+  getListing,
+  getAuction,
+  getListingImages,
+  getAuctionImages,
+  postSavedListing,
+  postSavedAuction,
 } from "../api/items";
 
 const ItemDetails = () => {
-	// get params from the url, assign to itemType and itemId
-	let { id } = useParams();
-	const params = id.split("id");
-	const itemType = params[0];
-	const itemId = params[1];
+  // get params from the url, assign to itemType and itemId
+  let { id } = useParams();
+  const params = id.split("id");
+  const itemType = params[0];
+  const itemId = params[1];
 
-	const [item, setItem] = useState({});
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState(null);
-	const [item_images, setImages] = useState([]);
-	const isAuction = itemType === "auction";
+  const [item, setItem] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
+  const [isItemSaved, setItemSaved] = useState(false);
+  const [error, setError] = useState(null);
+  const [item_images, setImages] = useState([]);
+  const isAuction = itemType === "auction";
 
-	useEffect(() => {
-		setIsLoading(true);
+  useEffect(() => {
+    setIsLoading(true);
 
-		const getItem =
-			itemType === "listing"
-				? getListing
-				: itemType === "auction"
-				? getAuction
-				: null;
-		const getImages =
-			itemType === "listing"
-				? getListingImages
-				: itemType === "auction"
-				? getAuctionImages
-				: null;
+    const getItem =
+      itemType === "listing"
+        ? getListing
+        : itemType === "auction"
+        ? getAuction
+        : null;
+    const getImages =
+      itemType === "listing"
+        ? getListingImages
+        : itemType === "auction"
+        ? getAuctionImages
+        : null;
 
-		if (!getItem || !getImages) {
-			setError(new Error("Invalid item type"));
-			setIsLoading(false);
-			return;
-		}
+    if (!getItem || !getImages) {
+      setError(new Error("Invalid item type"));
+      setIsLoading(false);
+      return;
+    }
 
-		getItem(itemId)
-			.then((data) => {
-				setItem(data);
+    getItem(itemId)
+      .then((data) => {
+        setItem(data);
 
-				getImages(itemId)
-					.then((data) => {
-						setImages(data);
-					})
-					.catch((error) => {
-						setError(error);
-					});
-			})
-			.catch((error) => {
-				setError(error);
-			})
-			.finally(() => {
-				setIsLoading(false);
-			});
-	}, [itemType, itemId]);
+        getImages(itemId)
+          .then((data) => {
+            setImages(data);
+          })
+          .catch((error) => {
+            setError(error);
+          });
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [itemType, itemId]);
 
-	if (isLoading) {
-		return null; // makes it less jarring when the page loads
-	}
+  const handleSave = async (itemId) => {
+    setIsSaving(true);
+    if (!localStorage.getItem("token")) {
+      alert("Please log in to save this item.");
+      return;
+    }
+    await (itemType === "listing"
+      ? postSavedListing(itemId)
+      : postSavedAuction(itemId));
+    setIsSaving(false);
+	alert("Item saved!");
+	setItemSaved(true);
+  };
 
-	if (error) {
-		return (
-			<div className="container mt-4">
-				<div className="alert alert-danger">Error: {error.message}</div>
-			</div>
-		);
-	}
+  if (isLoading) {
+    return null; // makes it less jarring when the page loads
+  }
 
-	return (
-		<div className="container mt-4">
-			<div className="card">
-				<div className="row g-0">
-					<div className="col">
-						{/* carousel of images, all container inside the listing_image table */}
-						<Carousel>
-							{[{ image_path: item.image_path }, ...item_images].map(
-								(image, index) => (
-									<Carousel.Item key={index}>
-										<img
-											className="d-block w-100 img-fluid rounded-start rounded-end"
-											src={image.image_path}
-											alt="Item"
-											style={{
-												objectFit: "contain",
-												maxHeight: "400px",
-												minHeight: "400px",
-											}}
-										/>
-									</Carousel.Item>
-								)
-							)}
-						</Carousel>
-					</div>
-					<div className="col col-5">
-						<div className="card-body">
-							<h5 className="card-title">{item.name}</h5>
-							<p className="card-text fs-4">
-								£{isAuction ? item.highest_bid : item.price} <br />
-								<small className="text-muted fs-6">or Best Offer</small>
-							</p>
-							<p className="card-text">
-								<small className="text-muted">
-									{isAuction ? (
-										<>
-											<Link
-												to="#"
-												style={{ textDecoration: "underline", color: "black" }}
-												onMouseEnter={(e) => (e.target.style.color = "grey")}
-												onMouseLeave={(e) => (e.target.style.color = "black")}
-											>
-												<AuctionBidCount auctionId={itemId} />
-											</Link>
-											<span className="mx-2">&bull;</span>
-											<Countdown
-												closingDate={item.closing_date}
-												isAuction={isAuction}
-											/>
-										</>
-									) : (
-										`Listed on ${new Date(item.created_at).toLocaleDateString(
-											"en-us",
-											{
-												weekday: "long",
-												year: "numeric",
-												month: "short",
-												day: "numeric",
-												hour: "numeric",
-												minute: "numeric",
-											}
-										)}`
-									)}
-								</small>
-							</p>
-							<p className="card-text">{item.description}</p>
-							<div className="input-group d-flex flex-column justify-content-between mt-auto">
-								<div>
-									<Button variant="primary" className="mb-2 d-block w-100">
-										<Link to="#" className="text-decoration-none text-white">
-											{isAuction ? "Submit Bid" : "Make Offer"}
-										</Link>
-									</Button>
-									<Button
-										variant="outline-primary"
-										className="mb-2 d-block w-100 text-primary"
-										style={{ backgroundColor: "white" }}
-										onMouseEnter={(e) =>
-											(e.target.style.backgroundColor = "#f2f2f2")
-										} // light gray
-										onMouseLeave={(e) =>
-											(e.target.style.backgroundColor = "white")
-										}
-									>
-										<Link to="#" className="text-decoration-none text-primary">
-											{"\u2661"}{" "}
-											{isAuction ? "Save this auction" : "Save this listing"}
-										</Link>
-									</Button>
-								</div>
-								<div className="align-self-end">
-									<Button variant="danger" className="btn-sm">
-										<Link to="#" className="text-decoration-none text-white">
-											{isAuction ? "Report Auction" : "Report Listing"}
-										</Link>
-									</Button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  if (error) {
+    return (
+      <div className="container mt-4">
+        <div className="alert alert-danger">Error: {error.message}</div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container mt-4">
+      <div className="card">
+        <div className="row g-0">
+          <div className="col">
+            {/* carousel of images, all container inside the listing_image table */}
+            <Carousel>
+              {[{ image_path: item.image_path }, ...item_images].map(
+                (image, index) => (
+                  <Carousel.Item key={index}>
+                    <img
+                      className="d-block w-100 img-fluid rounded-start rounded-end"
+                      src={image.image_path}
+                      alt="Item"
+                      style={{
+                        objectFit: "contain",
+                        maxHeight: "400px",
+                        minHeight: "400px",
+                      }}
+                    />
+                  </Carousel.Item>
+                )
+              )}
+            </Carousel>
+          </div>
+          <div className="col col-5">
+            <div className="card-body">
+              <h5 className="card-title">{item.name}</h5>
+              <p className="card-text fs-4">
+                £{isAuction ? item.highest_bid : item.price} <br />
+                <small className="text-muted fs-6">or Best Offer</small>
+              </p>
+              <p className="card-text">
+                <small className="text-muted">
+                  {isAuction ? (
+                    <>
+                      <Link
+                        to="#"
+                        style={{ textDecoration: "underline", color: "black" }}
+                        onMouseEnter={(e) => (e.target.style.color = "grey")}
+                        onMouseLeave={(e) => (e.target.style.color = "black")}
+                      >
+                        <AuctionBidCount auctionId={itemId} />
+                      </Link>
+                      <span className="mx-2">&bull;</span>
+                      <Countdown
+                        closingDate={item.closing_date}
+                        isAuction={isAuction}
+                      />
+                    </>
+                  ) : (
+                    `Listed on ${new Date(item.created_at).toLocaleDateString(
+                      "en-us",
+                      {
+                        weekday: "long",
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                        hour: "numeric",
+                        minute: "numeric",
+                      }
+                    )}`
+                  )}
+                </small>
+              </p>
+              <p className="card-text">{item.description}</p>
+              <div className="input-group d-flex flex-column justify-content-between mt-auto">
+                <div>
+                  <Button variant="primary" className="mb-2 d-block w-100">
+                    <Link to="#" className="text-decoration-none text-white">
+                      {isAuction ? "Submit Bid" : "Make Offer"}
+                    </Link>
+                  </Button>
+                  <Button
+                    variant="outline-primary"
+                    className="mb-2 d-block w-100 text-primary"
+                    style={{ backgroundColor: "white" }}
+                    onMouseEnter={(e) =>
+                      (e.target.style.backgroundColor = "#f2f2f2")
+                    } // light gray
+                    onMouseLeave={(e) =>
+                      (e.target.style.backgroundColor = "white")
+                    }
+					onClick={() => handleSave(itemId)}
+					disabled={isSaving}
+                  >
+                    <Link to="#" className="text-decoration-none text-primary">
+                      {"\u2661"}{" "}
+                      {isAuction ? (isItemSaved ? "Auction Saved!" : "Save this auction") : (isItemSaved ? "Listing Saved!" : "Save this listing")}
+                    </Link>
+                  </Button>
+                </div>
+                <div className="align-self-end">
+                  <Button variant="danger" className="btn-sm">
+                    <Link to="#" className="text-decoration-none text-white">
+                      {isAuction ? "Report Auction" : "Report Listing"}
+                    </Link>
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ItemDetails;
