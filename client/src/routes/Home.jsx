cea6abb4b7ff793695c2548f8117b748185410ef
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-03-11 21:03:29 +0000

    Removed unused useEffect and useState from Home.jsx

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Navbar from "../components/Navbar";
 import Search from "../components/Search";
 import Itemlist from "../components/Itemlist";
