1c08d649aa45a491fe39022c7aeb87aa237de7d1
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-03-11 21:03:04 +0000

    Reformatted with Prettier (2 spaces)

--- client/src/components/SavedItemList.jsx
+++ client/src/components/SavedItemList.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { getSavedAuction } from "../api/items";
-import { getSavedListing } from "../api/items";
+import { getSavedAuctions } from "../api/items";
+import { getSavedListings } from "../api/items";
 import { useNavigate } from "react-router-dom";
-
 
 const SavedItemList = () => {
   const [items, setItems] = useState([]);
@@ -11,65 +10,58 @@ const SavedItemList = () => {
   const navigate = useNavigate();
   const id = 1; // replace with the user id from the context
 
+  useEffect(() => {
+    setIsLoading(true);
+    Promise.all([getSavedListings(id), getSavedAuctions(id)])
+      .then(([listings, auctions]) => {
+        const mergedItems = [...listings, ...auctions];
+        mergedItems.sort(
+          (a, b) => new Date(b.created_at) - new Date(a.created_at)
+        ); //sort by created_at
+        setItems(mergedItems); //Set items
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
-useEffect(() => {
-  setIsLoading(true);
-  Promise.all([getSavedListing(id), getSavedAuction(id)])
-  .then(([listings, auctions]) => {
-    const mergedItems = [...listings, ...auctions];
-    mergedItems.sort(
-      (a, b) => new Date(b.created_at) - new Date(a.created_at)
-    );//sort by created_at
-    setItems(mergedItems); //Set items
-  })
-  .catch((error) => {
-    setError(error);
-  })
-  .finally(() => {
-    setIsLoading(false);
-  });
-}, []);
+  const handleCardClick = (key) => {
+    navigate(`/item/${key}`);
+  };
+  if (isLoading) {
+    return <div className="container text-center">Loading...</div>;
+  }
 
-const handleCardClick = (key) => {
-  navigate(`/item/${key}`)
-};
-if (isLoading) {
-  return <div className="container text-center">Loading...</div>;
-}
+  if (error) {
+    return (
+      <div className="container mt-4">
+        <div className="alert alert-danger">Error: {error.message}</div>
+      </div>
+    );
+  }
 
-if (error) {
   return (
-    <div className="container mt-4">
-      <div className="alert alert-danger">Error: {error.message}</div>
+    <div className="row" style={{ width: "50vw", marginLeft: "30vw" }}>
+      {items.map((item) => (
+        <div key={item.id} className="col-md-10 mt-4">
+          <div className="card">
+            <img src={item.image_path} alt={item.name} />
+            <div className="card-body col-md-5">
+              <h5 className="card-title">{item.name}</h5>
+              <p className="card-text">£{item.price || item.highest_bid}</p>
+            </div>
+
+            <button type="button" class="btn btn-outline-success">
+              Talk with Seller
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
-
-
-  return (
-        <div className="row" style={{ width: '50vw', marginLeft: '30vw'}}>
-          {items.map((item) => (
-            <div key={item.id} className="col-md-10 mt-4">
-              <div className="card">
-                <img
-                  src={item.image_path}
-                  alt={item.name}
-                />
-                <div className="card-body col-md-5">
-                  <h5 className="card-title">{item.name}</h5>
-                  <p className="card-text">£{item.price || item.highest_bid}</p>
-                </div>
-
-                <button type="button" class="btn btn-outline-success">Talk with Seller</button>
-
-                
-              </div>
-            </div>
-          ))}
-        </div> 
-  );
-};
-
-
 
 export default SavedItemList;
