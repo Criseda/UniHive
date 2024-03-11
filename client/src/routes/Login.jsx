2c69891b697d0e1cee3cea448db342f3c795cd65
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-03-11 21:03:42 +0000

    Removed Navbar from Login.jsx

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 import Loginbar from "../components/Loginbar";
 
 const Login = () => {
   return (
     <div>
-      {/* should this nav bar be here? users shouldn't be able to access site without logging in! */}
-      {/* <Navbar /> */}
       <Loginbar />
     </div>
   );
