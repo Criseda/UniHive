a90dc53976a0cd28f9f50932902d3300bad74ecb
m95647cw <chengrong.wu@student.manchester.ac.uk>
2024-03-12 08:51:37 +0000

    finsh editing avatar button

--- client/src/components/Profilebar.jsx
+++ client/src/components/Profilebar.jsx
@@ -7,28 +7,29 @@ const Profilebar = () => {
         <div className="container">
 
             <div  className="row justify-content-center mt-3">
-
-            
-                <button type="button" class="btn btn-white wd-25" data-toggle="modal" data-target="#exampleModal">
-                    <img src="/images/logo.jpg" class="img-thumbnail rounded-circle" alt="photo"/>
+                <button type="button" className="btn btn-white rounded-circle w-25 p-0" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <img src="/images/logo.jpg" className="img-thumbnail rounded-circle border-0 p-0" alt="photo"/>
                 </button>
 
 
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <div className="modal fade border-0" id="exampleModal" tabindex="-1" aria-labelledby="seting_avatar" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header border-0">
+                                <h5 className="modal-title text-center" id="seting_avatar">Set up your avatar</h5>
+                                <button type="button" className="close" data-bs-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
-                        <div class="modal-body">
-                            <h1>hhhhhhh</h1>
+                        <div className="modal-body border-0">
+                            <img src="/images/logo.jpg"  alt="photo"/>
+                            
+                            <input type="file" class="form-control" id="customFile" />
+
                         </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
+                            <div className="modal-footer border-0">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-primary">Save changes</button>
                             </div>
                         </div>
                     </div>
