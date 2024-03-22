b4b3d5263de31394e7194a68b30abe4ae2530088
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-03-22 17:25:20 +0000

    Updated environment variables for db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "unihive",
-  password: "unihiveftw",
-  host: "localhost",
-  port: 5432,
-  database: "unihive",
+  user: process.env.DB_USER || "unihive",
+  password: process.env.DB_PASSWORD || "unihiveftw",
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NAME || "unihive",
 });
 //the backend will connect to the database with these details
 
