0b807ad111208cf64a616d16780999cc66f0e317
a38062an <aduyanhn@gmail.com>
2024-03-27 22:23:57 +0000

    Implemented socket element to detect events from front end and sending back to front end for processing (socket.io shenanigans)

--- server/app.js
+++ server/app.js
@@ -6,10 +6,9 @@ const express = require("express");
 const session = require("express-session");
 const routes = require("./routes");
 const authRoutes = require("./auth/auth.js");
-
 const cors = require("cors");
-
 const app = express();
+const http = require("http").createServer(app)
 
 app.use(cors());
 app.use(express.json());
@@ -27,8 +26,39 @@ app.use(
 app.use("/api", routes);
 app.use("/auth", authRoutes);
 
-app.listen(5000, () => {
-	console.log("server is listening on port 5000");
+http.listen(5000, function() {
+  console.log("listening on port 5000");
 });
 
-//run the server with the command: nodemon app
+
+
+
+
+//setting up the socket.io for messages 
+const socketIO = require("socket.io")(http, {
+    cors: {
+        origin: "*",
+    },
+});
+
+//validiating the connection
+socketIO.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`)
+
+//when detecting send_message event send the message to the room (chat box)
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+//when detecting joinRoom event join the room
+  socket.on("joinRoom", ({ room }) => {
+    socket.join(room);
+
+
+
+
+
+  });
+  });
+});
+
+
+
