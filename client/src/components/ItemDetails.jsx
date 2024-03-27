794a315ac57321be803b7a5fec81afce7443b535
a38062an <aduyanhn@gmail.com>
2024-03-27 22:22:59 +0000

    Added functionality to the make a offer button in the itemdetails so that it will create a conversation by creating a room shared by the lister/auctioner and the current user (yourself).

--- client/src/components/ItemDetails.jsx
+++ client/src/components/ItemDetails.jsx
@@ -3,6 +3,10 @@ import Countdown from "./AuctionCountdown";
 import AuctionBidCount from "./AuctionBidCount";
 import { Carousel, Button } from "react-bootstrap";
 import { useParams, Link } from "react-router-dom";
+import {createMessageRoom} from "../api/items";
+import {getMessageRoomByUser} from "../api/items";
+import io from "socket.io-client";
+
 import {
   getListing,
   getAuction,
@@ -13,6 +17,9 @@ import {
   postSavedListing,
   postSavedAuction,
 } from "../api/items";
+
+const socket = io("http://localhost:5000");
+
 
 const ItemDetails = () => {
   // get params from the url, assign to itemType and itemId
@@ -114,6 +121,25 @@ const ItemDetails = () => {
     );
   }
 
+  const joinRoom = (id) => {
+   socket.emit("joinRoom", id);  
+  };
+
+  //This creates the message box 
+  const createMessage = async () => { 
+    const sellerId = item.seller_id;
+    console.log(sellerId);
+    createMessageRoom(sellerId); //This will add the room to the database
+    const roomId = await getMessageRoomByUser(sellerId);    
+    joinRoom(roomId); //This will join the user to the room      
+    
+  };
+
+  //function to submit a bid
+  const submitBid = () => {
+    console.log("submit bid");  
+  };
+
   return (
     <div className="container mt-4">
       <div className="card">
@@ -185,11 +211,22 @@ const ItemDetails = () => {
               <p className="card-text">{item.description}</p>
               <div className="input-group d-flex flex-column justify-content-between mt-auto">
                 <div>
-                  <Button variant="primary" className="mb-2 d-block w-100">
-                    <Link to="#" className="text-decoration-none text-white">
-                      {isAuction ? "Submit Bid" : "Make Offer"}
-                    </Link>
-                  </Button>
+    {/*Changed this conditional so that we can distinguish functions for auctions and listings*/}
+    {/*submitBid needs a page so messages is used as place hoder*/}
+                {isAuction ? (
+                     <Link to={isLoading ? "#" :"/messages"}className="text-decoration-none text-primary" onClick={submitBid}>
+                   <Button variant="primary" className="mb-2 d-block w-100" >
+                    {isLoading ? "loading..." : "Submit Bid"}
+                   </Button>
+                  </Link>
+                                 ) : (
+                  <Link to={isLoading ? "#" : "/messages"} className="text-decoration-none text-primary" onClick={createMessage}>
+                    <Button variant="primary" className="mb-2 d-block w-100" disabled={isLoading}>
+                      {isLoading ? "Loading..." : "Make Offer"} 
+                    </Button>
+                   </Link>
+                  
+                )} 
                   <Button
                     variant="outline-primary"
                     className="mb-2 d-block w-100 text-primary"
@@ -217,7 +254,7 @@ const ItemDetails = () => {
                 </div>
                 <div className="align-self-end">
                   <Button variant="danger" className="btn-sm">
-                    <Link to="#" className="text-decoration-none text-white">
+                    <Link to="#" className="text-decoroom: ration-none text-white">
                       {isAuction ? "Report Auction" : "Report Listing"}
                     </Link>
                   </Button>
