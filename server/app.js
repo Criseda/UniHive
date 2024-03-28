5a3ded4ee05cd5c61312e154c62d3252777418a5
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-03-28 16:02:27 +0000

    fixed formatting in app.js

--- server/app.js
+++ server/app.js
@@ -8,57 +8,45 @@ const routes = require("./routes");
 const authRoutes = require("./auth/auth.js");
 const cors = require("cors");
 const app = express();
-const http = require("http").createServer(app)
+const http = require("http").createServer(app);
 
 app.use(cors());
 app.use(express.json());
 app.use(
-	session({
-		secret: process.env.SESSION_SECRET, // change this to a random string in a .env file
-		resave: false,
-		saveUninitialized: true,
-		cookie: { secure: false },
-		// try set cookies to secure in production
-	})
+  session({
+    secret: process.env.SESSION_SECRET, // change this to a random string in a .env file
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+    // try set cookies to secure in production
+  })
 );
 
 // use the routes in the routes file
 app.use("/api", routes);
 app.use("/auth", authRoutes);
 
-http.listen(5000, function() {
-  console.log("listening on port 5000");
+http.listen(5000, () => {
+  console.log("server is listening on port 5000");
 });
 
-
-
-
-
-//setting up the socket.io for messages 
+//setting up the socket.io for messages
 const socketIO = require("socket.io")(http, {
-    cors: {
-        origin: "*",
-    },
+  cors: {
+    origin: "*",
+  },
 });
 
 //validiating the connection
 socketIO.on("connection", (socket) => {
-  console.log(`User connected: ${socket.id}`)
+  console.log(`User connected: ${socket.id}`);
 
-//when detecting send_message event send the message to the room (chat box)
+  //when detecting send_message event send the message to the room (chat box)
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
-//when detecting joinRoom event join the room
-  socket.on("joinRoom", ({ room }) => {
-    socket.join(room);
-
-
-
-
-
-  });
+    //when detecting joinRoom event join the room
+    socket.on("joinRoom", ({ room }) => {
+      socket.join(room);
+    });
   });
 });
-
-
-
