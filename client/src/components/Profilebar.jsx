6af51a51d6b309459e666b66bec0289c63addc15
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-04-04 19:23:13 +0100

    Changed Details columns to use more descriptive semantic elements, added useParams() for id parsing from url

--- client/src/components/Profilebar.jsx
+++ client/src/components/Profilebar.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { Button, Modal, Image, Container, Row, Col } from "react-bootstrap";
 import Stars from "./Star";
+import {useParams} from "react-router-dom";
 
 const Profilebar = () => {
   const [modalShow, setModalShow] = useState(false);
+  let { id } = useParams();
 
   return (
     <Container className="d-flex pt-4">
@@ -41,10 +43,13 @@ const Profilebar = () => {
       </Modal>
 
       <Col className="d-flex flex-column align-items-start p-2 text-break text-wrap">
-        <h1>User Name</h1>
-        <Stars starnumber={2.5}/>
+        <h2>Person's name here: {id}</h2>
+        <Stars starnumber={2.5} />
         <h3 className="pt-4">Description:</h3>
-        <p>I lfjah asdf ioromem lopsum eteclakdsfnjafsnldjkkljansdflkjansdflkajsndflkjasndflkajsndflkajsdfnlkjasndflkajsndflkajsndf</p>
+        <p>
+          I lfjah asdf ioromem lopsum
+          eteclakdsfnjafsnldjkkljansdflkjansdflkajsndflkjasndflkajsndflkajsdfnlkjasndflkajsndflkajsndf
+        </p>
       </Col>
     </Container>
   );
