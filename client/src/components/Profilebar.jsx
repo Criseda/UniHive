50833bc7f62aeaf26accc26abec8a4b0995ff971
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-04-06 19:00:15 +0100

    Profilebar now returns user data depending on the user id passed in

--- client/src/components/Profilebar.jsx
+++ client/src/components/Profilebar.jsx
@@ -1,11 +1,51 @@
-import React, { useState } from "react";
-import { Button, Modal, Image, Container, Row, Col } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Button, Modal, Image, Container, Col, Alert } from "react-bootstrap";
+import { getUser } from "../api/items";
 import Stars from "./Star";
-import {useParams} from "react-router-dom";
 
 const Profilebar = () => {
-  const [modalShow, setModalShow] = useState(false);
   let { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [modalShow, setModalShow] = useState(false);
+  const [user, setUser] = useState(null);
+  const [createdAt, setCreatedAt] = useState(null);
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [rating, setRating] = useState(null);
+  const [avatar, setAvatar] = useState(null);
+  const [banned, setBanned] = useState(null);
+
+  useEffect(() => {
+    getUser(id)
+      .then((data) => {
+        //remove this after testing
+        console.log(data);
+        // ---
+        setUser(data.id);
+        setCreatedAt(data.created_at);
+        setName(data.first_name + " " + data.last_name);
+        setBio(data.bio);
+        setRating(data.rating !== null ? data.rating : 0);
+        setAvatar(data.avatar_path);
+        setBanned(data.banned);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [id]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <Alert variant="danger">{error.toString()}</Alert>;
+  }
 
   return (
     <Container className="d-flex pt-4">
@@ -15,7 +55,7 @@ const Profilebar = () => {
         onClick={() => setModalShow(true)}
       >
         <Image
-          src="/images/logo.jpg"
+          src={avatar ? avatar : "/images/logo.jpg"}
           roundedCircle
           className="img-thumbnail border-0 p-0"
           alt="avatar"
@@ -31,7 +71,7 @@ const Profilebar = () => {
           <Modal.Title id="seting_avatar">Set up your avatar</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Image src="/images/logo.jpg" alt="photo" />
+          <Image src={avatar} alt="photo" />
           <input type="file" className="form-control" id="customFile" />
         </Modal.Body>
         <Modal.Footer>
@@ -43,12 +83,11 @@ const Profilebar = () => {
       </Modal>
 
       <Col className="d-flex flex-column align-items-start p-2 text-break text-wrap">
-        <h2>Person's name here: {id}</h2>
-        <Stars starnumber={2.5} />
-        <h3 className="pt-4">Description:</h3>
+        <h2>{name}</h2>
+        <Stars starnumber={rating} />
+        <h3 className="pt-4">Bio:</h3>
         <p>
-          I lfjah asdf ioromem lopsum
-          eteclakdsfnjafsnldjkkljansdflkjansdflkajsndflkjasndflkajsndflkajsdfnlkjasndflkajsndflkajsndf
+          {bio ? bio : "This user has not set up a bio yet."}
         </p>
       </Col>
     </Container>
