f95ca00db1f01f0a68013a4851da162a85c40b4b
Alex Mote <alexmote@admins-mbp-2.home>
2024-04-06 20:06:59 +0100

    Cleaned profile page

--- client/src/components/Profilebar.jsx
+++ client/src/components/Profilebar.jsx
@@ -1,52 +1,74 @@
 import React from "react";
 
-
 const Profilebar = () => {
-    return (
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-3">
+        <button
+          type="button"
+          class="btn btn-white wd-25"
+          data-toggle="modal"
+          data-target="#exampleModal"
+        >
+          <img
+            src="/images/logo.jpg"
+            class="img-thumbnail rounded-circle"
+            alt="photo"
+          />
+        </button>
 
-        <div className="container">
-
-            <div  className="row justify-content-center mt-3">
-
-            
-                <button type="button" class="btn btn-white wd-25" data-toggle="modal" data-target="#exampleModal">
-                    <img src="/images/logo.jpg" class="img-thumbnail rounded-circle" alt="photo"/>
+        <div
+          class="modal fade"
+          id="exampleModal"
+          tabindex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                  Modal title
+                </h5>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
                 </button>
-
-
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                        <div class="modal-body">
-                            <h1>hhhhhhh</h1>
-                        </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+              </div>
+              <div class="modal-body">
+                <h1>hhhhhhh</h1>
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" class="btn btn-primary">
+                  Save changes
+                </button>
+              </div>
             </div>
- 
-
-            <div className="row text-center mt-2">
-                <p>User Name</p>
-            </div>
-
-            <div className="row text-center mt-3">
-                <p>Email address</p>
-            </div>
-            <p className="mt-3">Post items</p>
+          </div>
         </div>
-    );
-  };
-  
-  export default Profilebar;
+      </div>
+
+      <div className="row text-center mt-2">
+        <p>User Name</p>
+      </div>
+
+      <div className="row text-center mt-3">
+        <p>Email address</p>
+      </div>
+      <p className="mt-3">Post items</p>
+    </div>
+  );
+};
+
+export default Profilebar;
