edc3e60fdd2596e4cf7c425855d94b27bf86dbb3
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-04-07 12:15:34 +0100

    Provided Profilebar a user_id for conditional rendering instead of useParams, which is now used in the parent component

--- client/src/components/Profilebar.jsx
+++ client/src/components/Profilebar.jsx
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import { Button, Modal, Image, Container, Col, Alert } from "react-bootstrap";
 import { getUser } from "../api/items";
 import Stars from "./Star";
 
-const Profilebar = () => {
-  let { id } = useParams();
+const Profilebar = ({ user_id }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [modalShow, setModalShow] = useState(false);
-  const [user, setUser] = useState(null);
   const [createdAt, setCreatedAt] = useState(null);
   const [name, setName] = useState("");
   const [bio, setBio] = useState("");
@@ -18,13 +15,13 @@ const Profilebar = () => {
   const [banned, setBanned] = useState(null);
 
   useEffect(() => {
-    getUser(id)
+    getUser(user_id)
       .then((data) => {
-        //remove this after testing
-        console.log(data);
-        // ---
-        setUser(data.id);
-        setCreatedAt(data.created_at);
+        const date = new Date(data.created_at);
+        const options = { year: "numeric", month: "long", day: "numeric" };
+        const formattedDate = date.toLocaleDateString(undefined, options);
+
+        setCreatedAt(formattedDate);
         setName(data.first_name + " " + data.last_name);
         setBio(data.bio);
         setRating(data.rating !== null ? data.rating : 0);
@@ -37,7 +34,7 @@ const Profilebar = () => {
       .finally(() => {
         setIsLoading(false);
       });
-  }, [id]);
+  }, [user_id]);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -52,47 +49,55 @@ const Profilebar = () => {
   }
 
   return (
-    <Container className="d-flex pt-4">
-      <Button
-        variant="white"
-        className="rounded-circle w-25 p-2"
-        onClick={() => setModalShow(true)}
-      >
-        <Image
-          src={avatar ? avatar : "/images/logo.jpg"}
-          roundedCircle
-          className="img-thumbnail border-0 p-0"
-          alt="avatar"
-        />
-      </Button>
+    <>
+      <Container className="d-flex pt-4">
+        <Button
+          variant="white"
+          className="rounded-circle w-25 p-2"
+          onClick={() => setModalShow(true)}
+        >
+          <Image
+            src={avatar ? avatar : "/images/logo.jpg"}
+            roundedCircle
+            className="img-thumbnail border-0 p-0"
+            alt="avatar"
+          />
+        </Button>
 
-      <Modal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        aria-labelledby="seting_avatar"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="seting_avatar">Set up your avatar</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Image src={avatar} alt="photo" />
-          <input type="file" className="form-control" id="customFile" />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={() => setModalShow(false)}>
-            Close
-          </Button>
-          <Button variant="primary">Save changes</Button>
-        </Modal.Footer>
-      </Modal>
+        <Modal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          aria-labelledby="seting_avatar"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="seting_avatar">Set up your avatar</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Image src={avatar} alt="photo" />
+            <input type="file" className="form-control" id="customFile" />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => setModalShow(false)}>
+              Close
+            </Button>
+            <Button variant="primary">Save changes</Button>
+          </Modal.Footer>
+        </Modal>
 
-      <Col className="d-flex flex-column align-items-start p-2 text-break text-wrap">
-        <h2>{name}</h2>
-        <Stars starnumber={rating} />
-        <h3 className="pt-4">Bio:</h3>
-        <p>{bio ? bio : "This user has not set up a bio yet."}</p>
-      </Col>
-    </Container>
+        <Col className="d-flex flex-column align-items-start p-2 text-break text-wrap">
+          <h2 className="m-0">{name}</h2>
+          <p className="m-0 text-muted small font-weight-normal">
+            Member since: {createdAt}
+          </p>
+          <div className="pt-2">
+            <Stars starnumber={rating} mobileSize={"lg"} size={"2x"} />
+          </div>
+          <h3 className="pt-4">Bio:</h3>
+          <p>{bio ? bio : "This user has not set up a bio yet."}</p>
+        </Col>
+      </Container>
+      <h4 className="pt-3 text-center h4 font-weight-bold">Posted items</h4>
+    </>
   );
 };
 
