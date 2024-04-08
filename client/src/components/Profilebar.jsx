300ec37303a03b28bdd57388104a7260e2c2a011
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-04-08 19:58:54 +0100

    Added check if the profile is the user logged in. If so, allow user to edit their profile with EditProfileModal.jsx

--- client/src/components/Profilebar.jsx
+++ client/src/components/Profilebar.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Button, Modal, Image, Container, Col, Alert } from "react-bootstrap";
-import { getUser } from "../api/items";
+import { Button, Image, Container, Col, Alert } from "react-bootstrap";
+import { getUser, getLoggedInUser } from "../api/items";
 import Stars from "./Star";
+import EditProfileModal from "./EditProfileModal";
 
 const Profilebar = ({ user_id }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [modalShow, setModalShow] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const [createdAt, setCreatedAt] = useState(null);
   const [name, setName] = useState("");
   const [bio, setBio] = useState("");
   const [rating, setRating] = useState(null);
   const [avatar, setAvatar] = useState(null);
   const [banned, setBanned] = useState(null);
+  const [isOwnAccount, setIsOwnAccount] = useState(false);
 
   useEffect(() => {
     getUser(user_id)
@@ -27,6 +29,12 @@ const Profilebar = ({ user_id }) => {
         setRating(data.rating !== null ? data.rating : 0);
         setAvatar(data.avatar_path);
         setBanned(data.banned);
+
+        getLoggedInUser().then((user) => {
+          if (user && user.id === user_id) {
+            setIsOwnAccount(true);
+          }
+        });
       })
       .catch((error) => {
         setError(error);
@@ -51,41 +59,19 @@ const Profilebar = ({ user_id }) => {
   return (
     <>
       <Container className="d-flex pt-4">
-        <Button
-          variant="white"
-          className="rounded-circle w-25 p-2"
-          onClick={() => setModalShow(true)}
-        >
+        <Col xs="auto" className="p-3">
           <Image
             src={avatar ? avatar : "/images/logo.jpg"}
             roundedCircle
-            className="img-thumbnail border-0 p-0"
+            fluid
             alt="avatar"
           />
-        </Button>
-
-        <Modal
-          show={modalShow}
-          onHide={() => setModalShow(false)}
-          aria-labelledby="seting_avatar"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="seting_avatar">Set up your avatar</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Image src={avatar} alt="photo" />
-            <input type="file" className="form-control" id="customFile" />
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={() => setModalShow(false)}>
-              Close
-            </Button>
-            <Button variant="primary">Save changes</Button>
-          </Modal.Footer>
-        </Modal>
+        </Col>
 
         <Col className="d-flex flex-column align-items-start p-2 text-break text-wrap">
-          <h2 className="m-0">{name}</h2>
+          <h2 className="m-0">
+            {name} {isOwnAccount && "(You)"}
+          </h2>
           <p className="m-0 text-muted small font-weight-normal">
             Member since: {createdAt}
           </p>
@@ -93,8 +79,31 @@ const Profilebar = ({ user_id }) => {
             <Stars starnumber={rating} mobileSize={"lg"} size={"2x"} />
           </div>
           <h3 className="pt-4">Bio:</h3>
-          <p>{bio ? bio : "This user has not set up a bio yet."}</p>
+          <pre>{bio ? bio : "This user has not set up a bio yet."}</pre>
         </Col>
+
+        {isOwnAccount && (
+          //Edit profile button
+          <Col xs="auto">
+            <Button
+              variant="secondary"
+              className="m-2 mt-3"
+              onClick={() => setShowModal(true)}
+            >
+              Edit profile
+            </Button>
+          </Col>
+        )}
+        {isOwnAccount && (
+          <EditProfileModal
+            showModal={showModal}
+            setShowModal={setShowModal}
+            bio={bio}
+            avatar={avatar}
+            setBio={setBio}
+            setAvatar={setAvatar}
+          />
+        )}
       </Container>
       <h4 className="pt-3 text-center h4 font-weight-bold">Posted items</h4>
     </>
