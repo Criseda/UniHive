068c9c60158a8b03801eba664cd024da28a176ed
Alex Mote <alexmote@admins-MacBook-Pro-2.local>
2024-04-10 13:55:33 +0100

    Added filters to search

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -4,6 +4,19 @@ const Search = () => {
   return (
     <form className="container mt-3 mb-3">
       <div className="input-group justify-content-center">
+        <div className="input-group-prepend">
+          <select
+            className="form-select"
+            aria-label="Default select example"
+            style={{ width: "150px !important" }} // Adjusted width here
+          >
+            <option value="0" selected>
+              All items
+            </option>
+            <option value="1">Fixed Price</option>
+            <option value="2">Auctions</option>
+          </select>
+        </div>
         <input
           type="text"
           className="form-control"
