b6f23d9f070149ed18f5abd3220247cb38ca4309
p12289lp <laurentiu-cristian.preda@student.manchester.ac.uk>
2024-04-10 20:02:53 +0100

    Removed Navbar as it has been replaced by Layout component

--- client/src/routes/Profilepage.jsx
+++ client/src/routes/Profilepage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import Navbar from "../components/Navbar";
 import Itemlist from "../components/Itemlist";
 import Profilebar from "../components/Profilebar"
 
@@ -9,7 +8,6 @@ const Profilepage = () => {
 
     return (
       <div>
-        <Navbar />
         <Profilebar user_id={id}/>
         <Itemlist user_id={id}/>
       </div>
